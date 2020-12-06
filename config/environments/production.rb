f85d3e2e87bc50ece0d99042282dd4b97e2a6c24
masafumikazama <masanomaildesu@yahoo.co.jp>
2020-12-06 11:09:06 +1000

    herokuデプロイのためproduction.rbのconfi.active_storageをコメントアウト

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
-  config.active_storage.service = :local
+  # config.active_storage.service = :local  (heroku push のためコメントアウト)
 
   config.action_mailer.default_url_options = { host: 'subscription-mall.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
