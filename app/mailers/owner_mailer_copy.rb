f5e3c3afbcaaa32e76e7100f4b3e57a601a379a8
ruffini47 <ruffini47@gmail.com>
2021-01-12 20:57:47 +0900

    Delete unnecessary file

--- app/mailers/owner_mailer_copy.rb
+++
@@ -1,9 +0,0 @@
-class OwnerMailer < ApplicationMailer
-
-  def owner_email(user, owner)
-    @user = user
-    @owner = owner
-    mail(:to => @owner.email, :subject => '#{@owner.name}様からメッセージが届きました')
-  end
-  
-end
--- spec/mailers/previews/owner_mailer_preview_copy2.rb
+++
@@ -1,9 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/owner_mailer
-class OwnerMailerPreview < ActionMailer::Preview
-
-  # Preview this email at http://localhost:3000/rails/mailers/owner_mailer/registration_confirmation
-  def registration_confirmation
-    OwnerMailer.registration_confirmation
-  end
-
-end
