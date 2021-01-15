741812aeb7b66f3f05db27276dd5eee68bbda9cd
suzuki-yoshiki <yoshisu04183@gmail.com>
2021-01-15 11:23:22 +0900

    edit2

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -13,9 +13,9 @@ class StaticPagesController < ApplicationController
     gon.maps = Map.all
     @categories_name = Category.where.not(name: nil)#検索機能が選択ボックスだったら使う
     @categories = if params[:search]
-      Category.search(params[:search]).order("RAND()").limit(6)
+      Category.search(params[:search]).order("RANDOM()").limit(6)
     else 
-      Category.order("RAND()").limit(6)
+      Category.order("RANDOM()").limit(6)
     end
   end
 
