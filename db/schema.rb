fe453579e1382bb3e0f5c46762031b69f91dd2f9
ruffini47 <ruffini47@gmail.com>
2021-01-28 23:13:33 +0900

    subscriptionsテーブルにadressカラムが２つ存在するエラーを解消

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210123230814) do
+ActiveRecord::Schema.define(version: 20210122235346) do
 
   create_table "admins", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "email", default: "", null: false
