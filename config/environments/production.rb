3c3919bc31e722c49bb0b5daae42e22302f22509
suzuki-yoshiki <yoshisu04183@gmail.com>
2021-01-31 22:35:12 +0900

    edit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options)
   # config.active_storage.service = :local  (heroku push のためコメントアウト)
 
-  config.action_mailer.default_url_options = { host: 'subscription-mall.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'www.megurumee.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
