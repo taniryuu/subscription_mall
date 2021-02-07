513fef03bbd81efb187ed7201b47206f88697286
takano-git <p.aaattt25@gmail.com>
2021-02-07 11:16:47 +0000

    add users user_price カラム

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210206100009) do
+ActiveRecord::Schema.define(version: 20210207110503) do
 
   create_table "admins", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "email", default: "", null: false
@@ -271,6 +271,7 @@ ActiveRecord::Schema.define(version: 20210206100009) do
     t.date "issue_ticket_day"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_price"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
