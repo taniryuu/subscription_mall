4fd7931a3fb8900f34f608a5c402ae1edf230876
ruffini47 <ruffini47@gmail.com>
2021-02-07 16:54:55 +0900

    サイトのフッター部分に「会社概要」を付け加え

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -271,6 +271,8 @@ class SubscriptionsController < ApplicationController
   def show_sample
   end
 
+  def company_profile
+  end
 
   private
     # Use callbacks to share common setup or constraints between actions.
