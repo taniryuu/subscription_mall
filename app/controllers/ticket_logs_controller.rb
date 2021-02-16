71a4e7c4d05478569cabba8f303c5de05b88a63c
takano-git <p.aaattt25@gmail.com>
2021-02-16 10:23:14 +0000

    コンフリクトを解消

--- app/controllers/ticket_logs_controller.rb
+++ app/controllers/ticket_logs_controller.rb
@@ -1,13 +1,9 @@
 class TicketLogsController < ApplicationController
   def index
     if current_user.present?
-<<<<<<< HEAD
       @ticket_logs = TicketLog.where(user_id: current_user.id)
     elsif current_owner.present?
       @ticket_logs = TicketLog.where(owner_email: current_owner.email)
-=======
-      @ticket_logs = TicketLog.where(ticket_id: Ticket.find_by(user_id: current_user.id).id)
->>>>>>> 26630e464680f39b7b26ee905c89e5738f771549
     elsif current_admin.present?
       @ticket_logs = TicketLog.all
     end
