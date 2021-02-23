178748fb1727eefda8d4b26f9532a5bec88936d2
ruffini47 <ruffini47@gmail.com>
2021-02-24 00:14:58 +0900

    private store upload時の写真アップロード機能実装したが未確認

--- app/controllers/private_stores_controller.rb
+++ app/controllers/private_stores_controller.rb
@@ -61,13 +61,13 @@ class PrivateStoresController < ApplicationController
     @private_store = PrivateStore.new(private_store_params)
     respond_to do |format|
       if @private_store.save
-	if params[:private_store][:qr_image]
-          File.binwrite("public/private_store_images/#{@private_store.id}.PNG", params[:private_store][:qr_image].read)
-          @private_store.update(qr_image: "#{@private_store.id}.PNG" )
+        if params[:private_store][:qr_image]
+          File.binwrite("public/private_store_images/#{@private_store.id}0.PNG", params[:private_store][:qr_image].read)
+          @private_store.update(qr_image: "#{@private_store.id}0.PNG" )
         else
           flash[:danger] = "QRコードを指定できませんでした。"
         end
-        format.html { redirect_to owner_private_stores_url(@private_store, id: @owner.id, owner_id: @owner.id), notice: 'サブスクショップを開設しました' }
+        format.html { redirect_to owner_private_stores_url(@private_store, id: @owner.id, owner_id: @owner.id), notice: 'サブスクショップを開>設しました' }
         format.json { render :show, status: :created, location: @private_store }
       else
         format.html { render :new }
@@ -75,17 +75,118 @@ class PrivateStoresController < ApplicationController
       end
     end
   end
-
   # PATCH/PUT /private_stores/1
   # PATCH/PUT /private_stores/1.json
   def update
     respond_to do |format|
       if @private_store.update(private_store_params)
 	if params[:private_store][:qr_image]
-          File.binwrite("public/private_store_images/#{@private_store.id}.PNG", params[:private_store][:qr_image].read)
-          @private_store.update(qr_image: "#{@private_store.id}.PNG" )
+          File.binwrite("public/private_store_images/#{@private_store.id}0.PNG", params[:private_store][:qr_image].read)
+          @private_store.update(qr_image: "#{@private_store.id}0.PNG" )
         else
           flash[:danger] = "QRコードを指定できませんでした。"
+        end
+	if params[:private_store][:image_private_store]
+          File.binwrite("public/private_store_images/#{@private_store.id}1.PNG", params[:private_store][:image_private_store].read)
+          @private_store.update(image_private_store: "#{@private_store.id}1.PNG" )
+        else
+          flash[:danger] = "image_private_storeを指定できませんでした。"
+        end
+        if params[:private_store][:image_private_store2]
+          File.binwrite("public/private_store_images/#{@private_store.id}2.PNG", params[:private_store][:image_private_store2].read)
+          @private_store.update(image_private_store2: "#{@private_store.id}2.PNG" )
+        else
+          flash[:danger] = "image_private_store2を指定できませんでした。"
+        end
+        if params[:private_store][:image_private_store3]
+          File.binwrite("public/private_store_images/#{@private_store.id}3.PNG", params[:private_store][:image_private_store3].read)
+          @private_store.update(image_private_store3: "#{@private_store.id}3.PNG" )
+        else
+          flash[:danger] = "image_private_store3を指定できませんでした。"
+        end
+        if params[:private_store][:image_private_store4]
+          File.binwrite("public/private_store_images/#{@private_store.id}4.PNG", params[:private_store][:image_private_store4].read)
+          @private_store.update(image_private_store4: "#{@private_store.id}4.PNG" )
+        else
+          flash[:danger] = "image_private_store4を指定できませんでした。"
+        end
+        if params[:private_store][:image_private_store5]
+          File.binwrite("public/private_store_images/#{@private_store.id}5.PNG", params[:private_store][:image_private_store5].read)
+          @private_store.update(image_private_store5: "#{@private_store.id}5.PNG" )
+        else
+          flash[:danger] = "image_private_store5を指定できませんでした。"
+        end
+        if params[:private_store][:sub_image]
+          File.binwrite("public/private_store_images/#{@private_store.id}6.PNG", params[:private_store][:sub_image].read)
+          @private_store.update(image_private_store: "#{@private_store.id}6.PNG" )
+        else
+          flash[:danger] = "sub_imageを指定できませんでした。"
+        end
+        if params[:private_store][:sub_image2]
+          File.binwrite("public/private_store_images/#{@private_store.id}7.PNG", params[:private_store][:sub_image2].read)
+          @private_store.update(image_private_store: "#{@private_store.id}7.PNG" )
+        else
+          flash[:danger] = "sub_image2を指定できませんでした。"
+        end
+	if params[:private_store][:sub_image3]
+          File.binwrite("public/private_store_images/#{@private_store.id}8.PNG", params[:private_store][:sub_image3].read)
+          @private_store.update(image_private_store: "#{@private_store.id}8.PNG" )
+        else
+          flash[:danger] = "sub_image3を指定できませんでした。"
+        end
+        if params[:private_store][:sub_image4]
+          File.binwrite("public/private_store_images/#{@private_store.id}9.PNG", params[:private_store][:sub_image4].read)
+          @private_store.update(image_private_store: "#{@private_store.id}9.PNG" )
+        else
+          flash[:danger] = "sub_image8を指定できませんでした。"
+        end
+        if params[:private_store][:sub_image5]
+          File.binwrite("public/private_store_images/#{@private_store.id}10.PNG", params[:private_store][:sub_image5].read)
+          @private_store.update(image_private_store: "#{@private_store.id}10.PNG" )
+        else
+          flash[:danger] = "sub_image5を指定できませんでした。"
+        end
+        if params[:private_store][:sub_image6]
+          File.binwrite("public/private_store_images/#{@private_store.id}11.PNG", params[:private_store][:sub_image6].read)
+          @private_store.update(image_private_store: "#{@private_store.id}11.PNG" )
+        else
+          flash[:danger] = "sub_image6を指定できませんでした。"
+        end
+        if params[:private_store][:sub_image7]
+          File.binwrite("public/private_store_images/#{@private_store.id}12.PNG", params[:private_store][:sub_image7].read)
+          @private_store.update(image_private_store: "#{@private_store.id}12.PNG" )
+        else
+          flash[:danger] = "sub_image7を指定できませんでした。"
+        end
+        if params[:private_store][:sub_image8]
+          File.binwrite("public/private_store_images/#{@private_store.id}13.PNG", params[:private_store][:sub_image8].read)
+          @private_store.update(image_private_store: "#{@private_store.id}13.PNG" )
+        else
+          flash[:danger] = "sub_image8を指定できませんでした。"
+        end
+        if params[:private_store][:sub_image9]
+          File.binwrite("public/private_store_images/#{@private_store.id}14.PNG", params[:private_store][:sub_image9].read)
+          @private_store.update(image_private_store: "#{@private_store.id}14.PNG" )
+        else
+          flash[:danger] = "sub_image9を指定できませんでした。"
+        end
+	if params[:private_store][:sub_image10]
+          File.binwrite("public/private_store_images/#{@private_store.id}15.PNG", params[:private_store][:sub_image10].read)
+          @private_store.update(image_private_store: "#{@private_store.id}15.PNG" )
+        else
+          flash[:danger] = "sub_image10を指定できませんでした。"
+        end
+        if params[:private_store][:sub_image11]
+          File.binwrite("public/private_store_images/#{@private_store.id}16.PNG", params[:private_store][:sub_image11].read)
+          @private_store.update(image_private_store: "#{@private_store.id}16.PNG" )
+        else
+          flash[:danger] = "sub_image10を指定できませんでした。"
+        end
+        if params[:private_store][:sub_image12]
+          File.binwrite("public/private_store_images/#{@private_store.id}17.PNG", params[:private_store][:sub_image12].read)
+          @private_store.update(image_private_store: "#{@private_store.id}17.PNG" )
+        else
+          flash[:danger] = "sub_image12を指定できませんでした。"
         end
         format.html { redirect_to owner_private_store_url(@private_store, owner_id: @owner.id), notice: 'サブスクショップを更新しました' }
         format.json { render :show, status: :ok, location: @private_store }
