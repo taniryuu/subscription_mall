8ce97349f65db5bf71ed05cae098c2ed44cf1fca
suzuki-yoshiki <yoshisu04183@gmail.com>
2021-03-01 08:44:45 +0900

    ログイン状態デザイン修正、category#indexのコーディング

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,18 @@ module ApplicationHelper
       page_name + " | " + base_title
     end
   end
+  # 渡された管理者がログイン済みの管理者であればtrueを返します。
+  def current_admin?(admin)
+    admin == current_admin
+  end
+
+  # 渡されたユーザーがログイン済みのユーザーであればtrueを返します。
+  def current_user?(user)
+    user == current_user
+  end
+
+  # 渡された経営者がログイン済みの経営者であればtrueを返します。
+  def current_owner?(owner)
+    owner == current_owner
+  end
 end
