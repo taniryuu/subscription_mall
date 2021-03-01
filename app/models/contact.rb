336144b4a95820ee1fa2f0b4248c2bdae9d7ed07
takano-git <p.aaattt25@gmail.com>
2021-03-01 07:57:55 +0000

    サポートフオームのkietawakusennwo

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,6 +3,6 @@ class Contact < ApplicationRecord
   # belongs_to :owner
   attr_accessor :name, :email, :message
 
-  validates :name, :presence => {:message => '名前を入力してください'}
+  validates :name, :presence => {:message => '名前を入力してください'}, length: { minimum: 2 }
   validates :email, :presence => {:message => 'メールアドレスを入力してください'}
 end
