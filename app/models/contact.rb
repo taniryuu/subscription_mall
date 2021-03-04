adbb6069d0a69478cd9dae9ab1dbd68b7bb6aa02
takano-git <p.aaattt25@gmail.com>
2021-03-04 14:10:12 +0000

    コンタクト　ブログ　サポートにバリデーションを追加

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -6,6 +6,6 @@ class Contact < ApplicationRecord
   validates :name, :presence => {:message => '名前を入力してください'}, length: { minimum: 2,:message => '名前を２文字以上で入力してください'}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, :presence => {:message => 'メールアドレスを入力してください'}, length: { maximum: 100 }, format: { with: VALID_EMAIL_REGEX,:message => '入力されたメールアドレスは不正な値です' }  
-  validates :message, :presence => {:message => 'お問い合わせ内容を入力してください'}
+  validates :message, :presence => {:message => 'お問い合わせ内容を入力してください'}, length: { maximum: 255, :message => 'お問い合わせ内容は255文字以内で書いてください' }
 
 end
