0f97ee74c1d28f18ec81128303b98e5e1b6b3f8d
takano-git <p.aaattt25@gmail.com>
2021-03-12 11:54:34 +0000

    contactで管理者へメール送信機能追加

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -5,4 +5,10 @@ class ContactMailer < ApplicationMailer
     @contact = contact
     mail(:to => contact.email, :subject => 'お問い合わせを承りました')
   end
+
+  # 管理者へお問い合わせが入った通知メール
+  def get_contact_email(contact)
+    @contact = contact
+    mail(:to => 'p.aaattt25@gmail.com', :subject => 'お問い合わせが入りました。ご確認をお願いいたします。')
+  end 
 end
