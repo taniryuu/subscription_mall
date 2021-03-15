7f31e2cfed341782c580b43ef50e307d9fd05af5
ruffini47 <ruffini47@gmail.com>
2021-03-16 00:23:22 +0900

    個人店舗、トップに1枚下に9枚にする

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210306095519) do
+ActiveRecord::Schema.define(version: 20210306142407) do
 
   create_table "admins", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "email", default: "", null: false
@@ -153,6 +153,7 @@ ActiveRecord::Schema.define(version: 20210306095519) do
     t.string "provider"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "info"
     t.index ["email"], name: "index_owners_on_email", unique: true
     t.index ["reset_password_token"], name: "index_owners_on_reset_password_token", unique: true
   end
@@ -184,10 +185,6 @@ ActiveRecord::Schema.define(version: 20210306095519) do
     t.text "private_store_detail"
     t.integer "shop_id"
     t.string "script"
-    t.string "image_private_store2"
-    t.string "image_private_store3"
-    t.string "image_private_store4"
-    t.string "image_private_store5"
     t.string "sub_image"
     t.string "sub_image2"
     t.string "sub_image3"
@@ -365,6 +362,7 @@ ActiveRecord::Schema.define(version: 20210306095519) do
     t.integer "session_price"
     t.integer "private_store_id"
     t.datetime "deleted_at"
+    t.string "info"
     t.index ["deleted_at"], name: "index_users_on_deleted_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
