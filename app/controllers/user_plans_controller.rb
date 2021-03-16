c1b84488dadf2f00572d88b9faadb162889466e6
ruffini47 <ruffini47@gmail.com>
2021-03-16 21:22:45 +0900

    設定をdevelopに戻す

--- app/controllers/user_plans_controller.rb
+++ app/controllers/user_plans_controller.rb
@@ -56,7 +56,7 @@ class UserPlansController < ApplicationController
       line_items: [{
         price_data: {
           currency: 'jpy',
-          product: 'prod_J40qfUcRXSInGo', #'prod_J3NbUHqtOpmfgT',
+          product: 'prod_J3NbUHqtOpmfgT',
           unit_amount: 1000,
           recurring: {interval: "month"}
         },
