9309630c9724a755742a3b19a826e3353a4e0030
takano-git <p.aaattt25@gmail.com>
2021-03-17 14:44:34 +0000

    Facebookアカウント作成でadminへメール送信成功

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -73,6 +73,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       if @user.persisted?
         flash[:notice] = I18n.t('devise.omniauth_callbacks.success', kind: provider.capitalize)
         sign_in_and_redirect @user, event: :authentication
+        UserMailer.with(user: @user).notice_user_joining_email.deliver_now
       else
         session["devise.#{provider}_data"] = request.env['omniauth.auth'].except("extra")
         redirect_to new_user_registration_url
