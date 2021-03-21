3c4ad95a4284a58c93f9f0360c889f3cc20d8b7f
suzuki-yoshiki <yoshisu04183@gmail.com>
2021-03-21 16:01:01 +0900

    sns-line-edit

--- app/controllers/owners/omniauth_callbacks_controller.rb
+++ app/controllers/owners/omniauth_callbacks_controller.rb
@@ -53,7 +53,6 @@ class Owners::OmniauthCallbacksController < Devise::OmniauthCallbacksController
         @profile = Owner.new(provider: @omniauth['provider'], uid: @omniauth['uid'])
         email = @omniauth['info']['email'] ? @omniauth['info']['email'] : "#{@omniauth['uid']}-#{@omniauth['provider']}@example.com"
         @profile = current_owner || Owner.create!(provider: @omniauth['provider'], uid: @omniauth['uid'], email: email, name: @omniauth['info']['name'], password: Devise.friendly_token[0, 20])
-        @profile.save(:validate => false)
         @profile.set_values(@omniauth)
         sign_in(:owner, @profile)
         # redirect_to edit_owner_path(@profile.user.id) and return
