e2d03b80cbb5c2e6944a3967c60dd85fb012e204
suzuki-yoshiki <yoshisu04183@gmail.com>
2021-03-30 16:55:46 +0900

    加盟店トライアルで「ない」場合までロジック組みテスト済み

--- db/migrate/20210216223000_add_recommend_to_private_stores.rb
+++ db/migrate/20210216223000_add_recommend_to_private_stores.rb
@@ -1,7 +1,7 @@
 class AddRecommendToPrivateStores < ActiveRecord::Migration[5.1]
   def change
     add_column :private_stores, :recommend, :boolean, default: true
-    add_column :private_stores, :category_private_stores_id, :integer
-    add_column :private_stores, :private_stores_id, :integer
+    add_column :private_stores, :category_private_store_id, :integer
+    add_column :private_stores, :private_store_id, :integer
   end
 end
