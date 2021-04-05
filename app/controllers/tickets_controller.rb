f3f91d61fe88a2269ab85c86dedc954ce845e92e
ruffini47 <ruffini47@gmail.com>
2021-04-06 00:00:55 +0900

    チケット履歴までOK_本番環境まだ

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -8,8 +8,12 @@ class TicketsController < ApplicationController
 
   def show
     @ticket = Ticket.find(params[:id])
-    @private_store = PrivateStore.find_by(params[:id])
-    @subscription = Subscription.find_by(params[:id])
+    #user = User.find(@ticket.user_id)
+    if @ticket.subscription_name.present? 
+      @subscription = Subscription.find_by(name: @ticket.subscription_name)
+    elsif @ticket.private_store_name.present?
+      @private_store = PrivateStore.find_by(name: @ticket.private_store_name)
+    end
   end
 
   def new
@@ -20,29 +24,30 @@ class TicketsController < ApplicationController
   def create
     @ticket = Ticket.new(ticket_params)
    if @ticket.save
-       flash[:success] = "チケットを発券しました"
-      redirect_to user_account_user_path(current_user), notice: 'チケットを発券しました。Myアカウントからチケット確認ボタンを押してチケットを使ってみましょう！'
+     current_user.update(issue_ticket_day: Date.today)
+     flash[:success] = "チケットを発券しました"
+     redirect_to user_account_user_path(current_user), notice: 'チケットを発券しました。Myアカウントからチケット確認ボタンを押してチケットを使ってみましょう！'
    else
-       flash[:danger] = "チケットが発券できませんでした"
-       redirect_to root_path
+     flash[:danger] = "チケットが発券できませんでした"
+     redirect_to root_path
    end
-   current_user.update(issue_ticket_day: Date.today)
+   #current_user.update(issue_ticket_day: Date.today)
   end
 
   # チケットを使うボタンを押した後の処理（使った日を入れる）（１回目以降ずっと）
   def update
     @ticket = Ticket.find_by(user_id: params[:user_id])
     # @ticket_log = TicketLog.new(ticket_id: @ticket.id, use_ticket_day_log: use_ticket_params)
-    if @ticket.trial_last_check == "参加"
-      ticket_trial = "トライアル"
-    else
-      ticket_trial = "トライアルではありません"
-    end  
     if @ticket.update_attributes(use_ticket_params)
+      if @ticket.trial_last_check == "参加"
+        ticket_trial = "利用"
+      else
+        ticket_trial = "-"
+      end
       TicketLog.create(use_ticket_day_log: @ticket.use_ticket_day, owner_name: @ticket.owner_name,
 		       owner_email: @ticket.owner_email, owner_phone_number: @ticket.owner_phone_number, owner_store_information: @ticket.owner_store_information,
 		       subscription_name: @ticket.subscription_name, private_store_name: @ticket.private_store_name, subscription_fee: @ticket.subscription_fee,
-		       issue_ticket_day: @ticket.issue_ticket_day,user_id: @ticket.user_id, price: @ticket.price, trial: @ticket.trial_last_check)
+		       issue_ticket_day: @ticket.issue_ticket_day,user_id: @ticket.user_id, price: @ticket.price, trial: ticket_trial)
       #TicketMailer.ticket_email(@ticket).deliver_now
       redirect_to ticket_success_path
     else
@@ -81,7 +86,7 @@ class TicketsController < ApplicationController
   private
 
     def ticket_params
-	    params.require(:ticket).permit(:owner_name, :owner_email, :owner_phone_number, :owner_store_information, :price, :trial, :trial_check, :trial_count, :subscription_name, :private_store_name, :subscription_fee, :issue_ticket_day, :user_id)
+	    params.require(:ticket).permit(:owner_name, :owner_email, :owner_phone_number, :owner_store_information, :price, :trial, :trial_check, :trial_last_check, :trial_count, :subscription_name, :private_store_name, :subscription_fee, :issue_ticket_day, :user_id)
     end
 
     def edit_user_ticket
