b677832dd2a44feb7655c24a40a2fd5932823f8d
tr101176 <tr76aquarius@gmail.com>
2021-04-11 07:53:18 +0900

    チケット発券関連の曜日エラーを修正

--- app/controllers/ticket_logs_controller.rb
+++ app/controllers/ticket_logs_controller.rb
@@ -4,7 +4,6 @@ class TicketLogsController < ApplicationController
       @ticket_logs = TicketLog.where(user_id: current_user.id)
     elsif current_owner.present?
       @ticket_logs = TicketLog.where(owner_email: current_owner.email)
-      @ticket_logs = TicketLog.where(ticket_id: Ticket.find_by(user_id: current_user.id).id)
     elsif current_admin.present?
       @ticket_logs = TicketLog.all
     end
